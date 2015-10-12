015b92847039de0f73401765d9d849b5fb38a2a5
Chris Roy <cmr223@miami.edu>
2015-10-12 14:36:29 -0400

    Hello Mars

---
+++ sketch.js
@@ -0,0 +1,11 @@
+//Create a sketch that uses input from a form to interact with the canvas.
+
+	function setup() {
+  // uncomment this line to make the canvas the full size of the window
+   createCanvas(windowWidth, windowHeight);
+}
+
+	function draw() {
+  // draw stuff here
+  ellipse(width/2, height/2, 50, 50);
+}
